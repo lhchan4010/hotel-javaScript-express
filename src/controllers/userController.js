233da16df17271c95e5ade5567a82bb1ba37e5ac
chan <lhchan4010@gmail.com>
2022-03-16 04:19:28 +0900

    find range revenue

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -12,18 +12,52 @@ export const getAccounting = async (req, res) => {
   });
 };
 
-export const getRevenue = async (req, res, next) => {
-  const { user: _id } = req.session;
-  const today = new Date();
-  const weekAgo = new Date(today - 604800000);
-  const checkInDatas = await CheckIn.find({
+const findRevenue = async (range, _id) => {
+  const checkInData = await CheckIn.find({
     owner: _id,
-    createAt: {
-      $gte: weekAgo.toISOString().slice(0, 10),
-    },
+    createAt: { $gte: range },
   });
-  res.status(200).send(checkInDatas);
+  return checkInData;
 };
+
+export const getRevenue = async (req, res, next) => {
+  const {
+    params: { range },
+    session: { user: _id },
+  } = req;
+  const date = new Date();
+  if (range === 'week') {
+    date.setDate(date.getDate() - 6);
+    const range = date.toISOString().slice(0, 10);
+    const revenue = await findRevenue(range, _id);
+    return res.status(200).send(revenue);
+  }
+  if (range === 'month') {
+    const range = date.toISOString().slice(0, 7);
+    const revenue = await findRevenue(range, _id);
+    return res.status(200).send(revenue);
+  }
+  if (range === 'year') {
+    const range = date.toISOString().slice(0, 4);
+    const revenue = await findRevenue(range, _id);
+    return res.status(200).send(revenue);
+  }
+
+  res.status(200);
+};
+
+// export const getRevenue = async (req, res, next) => {
+//   const { user: _id } = req.session;
+//   const today = new Date();
+//   const weekAgo = new Date(today - 604800000);
+//   const checkInDatas = await CheckIn.find({
+//     owner: _id,
+//     createAt: {
+//       $gte: weekAgo.toISOString().slice(0, 10),
+//     },
+//   });
+//   res.status(200).send(checkInDatas);
+// };
 
 export const getMonth = async (req, res) => {
   const { user: _id } = req.session;
